b83e4e1fdd5f20430b1ea756778e99ba7b5cd3fc
Adalid26Islas <adalidislasquintero@gmail.com>
2025-09-07 00:15:32 -0600

    Update

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -29,7 +29,7 @@ export default function AboutMe() {
         {/* Imagen con bordes redondeados + sombra neón */}
         <div className="flex justify-center">
           <img
-            src="../../src/assets/certs/adalid.png"
+            src="public\Adalid.png"
             alt="Foto de Adalid Islas Quintero"
             className="w-80 h-200 object-cover rounded-2xl shadow-[0_0_10px_2px_rgba(0,255,255,0.7)]"
           />
